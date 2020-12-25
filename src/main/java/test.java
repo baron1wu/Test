be40c675fa86b71aba058ea84eecc27971c45914
baron1wu <70894798+baron1wu@users.noreply.github.com>
2020-12-25 19:27:44 +0800

    Update test.java
    hot-fix commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("version1");
         System.out.println("lalla");
+        System.out.println("hot-fix");
     }
 }
